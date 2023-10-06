55f4ef350e5861f41c324f2ecdf34302742aef6c
Rodrigo Steimbach <rod3001@gmail.com>
2023-10-06 17:45:00 -0300

    Moving Pieces

--- src/chess/ChessExecption.java
+++ src/chess/ChessExecption.java
@@ -1,6 +1,8 @@
 package chess;
 
-public class ChessExecption extends RuntimeException {
+import boardgame.BoardException;
+
+public class ChessExecption extends BoardException {
 	private static final long serialVersionUID = 1L;
 	
 	public ChessExecption(String msg) {
